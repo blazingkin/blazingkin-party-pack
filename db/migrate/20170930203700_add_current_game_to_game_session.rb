5f6ec3516947f271515a364fc93c6dd8ba0f38e4
Alex Gravenor <blazingkin@gmail.com>
2017-10-19 22:21:30 -0700

    Remove another game data migration

--- db/migrate/20170930203700_add_current_game_to_game_session.rb
+++
@@ -1,8 +0,0 @@
-class AddCurrentGameToGameSession < ActiveRecord::Migration[5.0]
-  def change
-    change_table :game_data do |t|
-      t.string :game_type
-      t.belongs_to :game_session
-    end
-  end
-end
