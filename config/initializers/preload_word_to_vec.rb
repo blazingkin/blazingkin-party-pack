c86263f3963f17136c6fc1bb018e1c0fa81f6662
Alex Gravenor <blazingkin@gmail.com>
2020-01-01 15:21:47 -0800

    Only preload in server mode

--- config/initializers/preload_word_to_vec.rb
+++ config/initializers/preload_word_to_vec.rb
@@ -1,3 +1,5 @@
-p "Preloading word 2 vec"
-p WordToVecService.get_word
-p "Done"
+if Rails.env.development? || Rails.env.production?
+    p "Preloading word 2 vec"
+    p WordToVecService.get_word
+    p "Done"
+end
